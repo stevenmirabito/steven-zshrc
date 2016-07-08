e62337465711f93451faecb6118ff65e74137317
Marc Billow <mbillow@constantcontact.com>
2016-07-08 18:19:42 -0400

    Move to printf

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 ZSH_LOC=$(which zsh)
 ETC_SHELL=$(grep "$ZSH_LOC" /etc/shells)
 
-echo "              __    _ ____             
+printf "              __    _ ____             
    ____ ___  / /_  (_) / /___ _      __
   / __ \`__ \\\/ __ \\/ / / / __ \\ | /| / /
  / / / / / / /_/ / / / / /_/ / |/ |/ / 
@@ -12,26 +12,26 @@ Welcome to your new home! Follow the prompts.\n"
 
 if [[ "$ETC_SHELL" == "" ]]
 then
-	sudo echo "\n$ZSH_LOC" >> /etc/shells
-	echo "ZSH has been added to /etc/shells."
+	sudo printf "\n$ZSH_LOC" >> /etc/shells
+	printf "ZSH has been added to /etc/shells."
 fi
 
 if [[ "$SHELL" != "$ZSH_LOC" ]]
 then
 	chsh -s "$ZSH_LOC" $USER
-	echo "User \"$USER\"'s shell has been altered."
+	printf "User \"$USER\"'s shell has been altered."
 fi
 
-echo "Installing OMZSH..."
+printf "Installing OMZSH..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-echo "Pulling ZSH config and setting ZSH environment."
-echo "# Path to your oh-my-zsh installation.\nexport ZSH=$HOME/.oh-my-zsh\n" > ~/.zshrc
+printf "Pulling ZSH config and setting ZSH environment."
+printf "# Path to your oh-my-zsh installation.\nexport ZSH=$HOME/.oh-my-zsh\n" > ~/.zshrc
 curl --progress-bar "https://raw.githubusercontent.com/mbillow/right-at-home/master/configs/zshrc" >> ~/.zshrc
 
-echo "Grabbing ZSH theme and plugins."
+printf "Grabbing ZSH theme and plugins."
 curl --progress-bar "https://raw.githubusercontent.com/mbillow/right-at-home/master/theme/mbillow.zsh-theme" >> ~/.oh-my-zsh/themes/mbillow.zsh-theme
 git clone -q https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone -q https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-echo "\nYou should be all set, just restart your terminal emulator.\n"
+printf "\nYou should be all set, just restart your terminal emulator.\n"
