6a950d4ddd9d98b78bc7f812e248b4d6a67a92aa
Steven Mirabito <steven@stevenmirabito.com>
2016-07-10 17:40:42 -0400

    Updated setup.sh and zshrc, removed Marc's theme

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
-#Right at Home
-######A simple personal setup script to simplify the customization of new hosts.
+steven-zshrc
+=============
+###### A simple setup script to install my person ZSH config
 
-###To Run:
-`sh -c "$(curl -fsSL http://home.billow.me)"`
+Installation
+-------------
+If you trust me, here's a one-line installation:
 
-###What this script does:
+    curl -o- -fsSL https://setup.stevenmirabito.com | zsh
+
+What this script does:
+-----------------------
 1. Find the local ZSH installation.
 2. Checks if ZSH is in /etc/shells.
 3. Sets the user's shell to the detected ZSH installation.
 4. Installs Oh-My-ZSH.
-5. Pulls the RC File, theme, and any extensions.
+5. Pulls the RC File and any extensions.
--- setup.sh
+++ setup.sh
@@ -1,39 +1,78 @@
 #!/bin/zsh
-ZSH_LOC=$(which zsh)
-ETC_SHELL=$(grep "$ZSH_LOC" /etc/shells)
+# Installer for steven-zshrc
+# Author: Steven Mirabito (steven@stevenmirabito.com)
+# Author: Marc Billow (marc@billow.me)
 
-printf "              __    _ ____             
-   ____ ___  / /_  (_) / /___ _      __
-  / __ \`__ \\\/ __ \\/ / / / __ \\ | /| / /
- / / / / / / /_/ / / / / /_/ / |/ |/ / 
-/_/ /_/ /_/_.___/_/_/_/\\____/|__/|__/  
-                                       
-Welcome to your new home! Follow the prompts.\n"
+# Paths
+ZSH_LOC=`which zsh`
+ETC_SHELL=`grep "$ZSH_LOC" /etc/shells`
 
+if [ ! -n "$ZSH" ]; then
+    ZSH=~/.oh-my-zsh
+fi
 
-if [[ "$ETC_SHELL" == "" ]]
-then
+if [ ! -n "$ZSH_CUSTOM" ]; then
+    ZSH_CUSTOM="${ZSH}/custom"
+fi
+
+ZSH_HIGHLIGHT_PLUGIN="${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting"
+ZSH_SUGGEST_PLUGIN="${ZSH_CUSTOM}/plugins/zsh-autosuggestions"
+
+# Styles
+RESET="\e[0m"
+BOLD="\e[1m"
+LOGO="${BOLD}\e[92m"
+PRIMARY="${BOLD}\e[91m"
+
+echo -e "${LOGO}"
+echo "         __                                       __             "
+echo "   _____/ /____ _   _____  ____       ____  _____/ /_  __________"
+echo "  / ___/ __/ _ \ | / / _ \/ __ \_____/_  / / ___/ __ \/ ___/ ___/"
+echo " (__  ) /_/  __/ |/ /  __/ / / /_____// /_(__  ) / / / /  / /__  "
+printf "/____/\__/\___/|___/\___/_/ /_/      /___/____/_/ /_/_/   \___/\n\n${RESET}"
+
+if [[ "$ETC_SHELL" == "" ]]; then
 	sudo printf "\n$ZSH_LOC" >> /etc/shells
-	printf "ZSH has been added to /etc/shells."
+	echo -e "${PRIMARY}✅  ZSH has been added to /etc/shells.${RESET}"
 fi
 
 if [[ "$SHELL" != "$ZSH_LOC" ]]
 then
 	chsh -s "$ZSH_LOC" $USER
-	printf "User \"$USER\"'s shell has been altered."
+	echo -e "${PRIMARY}✅  User \"$USER\"'s shell has been altered.${RESET}"
 fi
 
-printf "Installing OMZSH..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+if [ -d "$ZSH" ]; then
+    echo -e "${PRIMARY}🛠  Upgrading Oh My Zsh...${RESET}"
+    sh -c "`curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/upgrade.sh`"
+else
+    echo -e "${PRIMARY}📥  Installing Oh My Zsh...${RESET}"
+    sh -c "`curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh`"
+fi
 
-printf "Pulling ZSH config and setting ZSH environment."
-printf "# Path to your oh-my-zsh installation.\nexport ZSH=$HOME/.oh-my-zsh\n" > ~/.zshrc
-curl --progress-bar "https://raw.githubusercontent.com/mbillow/right-at-home/master/configs/zshrc" >> ~/.zshrc
+echo -e "${PRIMARY}📦  Pulling ZSH config and setting ZSH environment.${RESET}"
+echo "# Path to your oh-my-zsh installation.\nexport ZSH=$HOME/.oh-my-zsh\n" > ~/.zshrc
+curl -fL --progress-bar "https://setup.stevenmirabito.com/configs/zshrc" >> ~/.zshrc
 
-printf "Grabbing ZSH theme and plugins."
-curl --progress-bar "https://raw.githubusercontent.com/mbillow/right-at-home/master/theme/mbillow.zsh-theme" > ~/.oh-my-zsh/themes/mbillow.zsh-theme
-git clone -q https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone -q https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+if [[ -d "${ZSH_HIGHLIGHT_PLUGIN}" ]]; then
+    echo -e "${PRIMARY}🖌  Updating zsh-syntax-highlighting...${RESET}"
+    cd ${ZSH_HIGHLIGHT_PLUGIN}
+    git fetch --all
+    git reset --hard origin/master
+else
+    echo -e "${PRIMARY}🖌  Installing zsh-syntax-highlighting...${RESET}"
+    git clone -q https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_HIGHLIGHT_PLUGIN}
+fi
+
+if [[ -d "${ZSH_SUGGEST_PLUGIN}" ]]; then
+    echo "${PRIMARY}🔎  Updating zsh-autosuggestions...${RESET}"
+    cd ${ZSH_SUGGEST_PLUGIN}
+    git fetch --all
+    git reset --hard origin/master
+else
+    echo "${PRIMARY}🔎  Installing zsh-autosuggestions...${RESET}"
+    git clone -q https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_SUGGEST_PLUGIN}
+fi
 
 source ~/.zshrc
-printf "\nYou should be all set!\n"
+printf "${PRIMARY}🏁  Done!${RESET}\n"
